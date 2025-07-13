79a3cb306ac825116fd8c09a1871efc7f79ad261
engr-faysal <engr.mdfaysal@gmail.com>
2025-07-13 23:02:18 +0600

    Synchronous Fs Module

--- fsModule.js
+++ fsModule.js
@@ -4,15 +4,24 @@ var http = require('http');
 const Server1 =  http.createServer(function(request, response) {
 if(request.url=="/"){
 
-    // Asynchronous Fs Module
+// Asynchronous Fs Module
+    /*
     fs.readFile('fsModule.html', function(err, data) {
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
         response.end();
-    })
+    })       */
+
+// Synchronous Fs Module
+
+       let myData =  fs.readFileSync('fsModule.html');
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(myData);
+        response.end();
 }
 })
 Server1.listen(5052);
 console.log('Server Run Successfully & port 5052')
 
 // localhost:5052
+// node fsModule.js
